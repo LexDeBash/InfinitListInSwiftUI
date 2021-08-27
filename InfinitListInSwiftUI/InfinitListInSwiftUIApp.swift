4cedb63ebac56eaa121be07068125097c97bc282
Alexey Efimov <lex.efimov@swiftbook.ru>
2021-08-27 15:48:50 +0300

    Initial Commit

---
+++ InfinitListInSwiftUI/InfinitListInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfinitListInSwiftUIApp.swift
+//  InfinitListInSwiftUI
+//
+//  Created by Alexey Efimov on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct InfinitListInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
