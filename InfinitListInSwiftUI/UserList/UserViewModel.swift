a815217bb11072e9a762f9bc47f0970fce0775b9
Alexey Efimov <lex.efimov@swiftbook.ru>
2021-08-29 17:01:34 +0300

    Create UserViewModel

---
+++ InfinitListInSwiftUI/UserList/UserViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  UserViewModel.swift
+//  UserViewModel
+//
+//  Created by Alexey Efimov on 29.08.2021.
+//
+
+import Combine
+import Foundation
+
+class UserViewModel {
+    @Published var users: [User] = []
+    @Published var isRequestFailed = false
+    
+    private let pageLimit = 25
+    private var currentLastId: Int?
+    private var cancellable: AnyCancellable?
+    
+    func getUsers() {
+        cancellable = NetworkManager.shared.getUsers(perPage: pageLimit, sinceId: currentLastId)
+            .receive(on: DispatchQueue.main)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("Finished")
+                case .failure(let error):
+                    self.isRequestFailed = true
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { users in
+                self.users.append(contentsOf: users)
+                self.currentLastId = users.last?.id
+            }
+    }
+}
